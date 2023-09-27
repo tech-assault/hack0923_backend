e2125e7396971b2f8e3d2d33e58a858f71bd4459
1 <margoloko@mail.ru>
2023-09-27 11:57:42 +0700

    feature: Добавлен фильтр для StoreViewSet

--- backend/demand_oracle/sales/views.py
+++ backend/demand_oracle/sales/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import filters, mixins, viewsets
 
 from .models import Category, Forecast, Sale, Store
 from .serializers import (
@@ -21,27 +21,31 @@ class StoreViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["type_format", "loc", "city", "division"]
 
 
 class SaleViewSet(viewsets.ReadOnlyModelViewSet):
     """Вьюсет для модели Sale."""
 
+    serializer_class = SaleSerializer
+
     def get_queryset(self):
-        """."""
+        """Возвращает набор данных продаж для заданного SKU и ID магазина."""
         sku = self.request.query_params.get("sku")
         store_id = self.request.query_params.get("store_id")
         return Sale.objects.filter(sku=sku, store_id=store_id)
 
-    serializer_class = SaleSerializer
 
-
-class ForecastViewSet(viewsets.ModelViewSet):
+class ForecastViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
     """Вьюсет для модели Forecast."""
 
+    serializer_class = ForecastSerializer
+
     def get_queryset(self):
-        """."""
+        """Возвращает набор данных прогнозов для заданных SKU и ID магазина."""
         sku = self.request.query_params.get("sku")
         store_id = self.request.query_params.get("store_id")
         return Forecast.objects.filter(sku=sku, store_id=store_id)
-
-    serializer_class = ForecastSerializer
