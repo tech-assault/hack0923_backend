c25b48c02835666ab019dcc3cd062509854f5ff6
1 <margoloko@mail.ru>
2023-10-04 18:11:23 +0700

    feature: Настроена админ панель

--- backend/forecast/users/admin.py
+++ backend/forecast/users/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
 from .models import User
 
-
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    """Административная панель для модели User."""
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', "first_name", "last_name", 'is_admin',)  # Список полей, отображаемых в списке записей
+    list_filter = ('is_admin', 'email', 'username',)
 
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    search_fields = ("username", "email")
+
--- backend/forecast/users/models.py
+++ backend/forecast/users/models.py
@@ -3,9 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    """."""
-
-    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = "Пользователь"
