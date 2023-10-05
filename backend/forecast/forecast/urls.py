76ec3d6d5a04c01b3b7e6ce5d23d86cf2d3ae56f
pauli <p0lzi@ya.ru>
2023-10-05 05:37:29 +0300

    Очистка от старых тестов

--- backend/forecast/forecast/urls.py
+++ backend/forecast/forecast/urls.py
@@ -5,10 +5,8 @@ from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
 
 from forecast import settings
-from sale.views import live_sql_editor
 
 urlpatterns = [
-    path('admin/live-editor/', live_sql_editor, name="sql_editor"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
