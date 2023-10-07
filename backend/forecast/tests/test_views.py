ec1b5da144b87895e37bc90eb66913df33ca074c
1 <margoloko@mail.ru>
2023-10-07 19:37:10 +0700

    add test: Тестирование StoreViewSet

--- backend/forecast/tests/test_views.py
+++ backend/forecast/tests/test_views.py
@@ -4,7 +4,7 @@ import sys
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
-from sale.models import Category
+from sale.models import Category, Store
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CODE_DIR_PATH = os.path.join(BASE_DIR, "api")
@@ -15,7 +15,7 @@ class CategoryAPITestCase(TestCase):
     """Тестирование CategoryViewSet."""
 
     def setUp(self):
-        """Метод для настройки объектов."""
+        """Создание клиента API и тестовых данных категорий."""
         self.client = APIClient()
         self.category_data = {
             "sku": "TestSKU",
@@ -27,20 +27,38 @@ class CategoryAPITestCase(TestCase):
         Category.objects.create(**self.category_data)
 
     def test_get_category_list(self):
-        """Тест получения списка категорий."""
+        """
+        Act: Получение списка категорий.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
         response = self.client.get("/api/categories/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
 
     def test_get_category_detail(self):
-        """Тест получения деталей категории."""
+        """
+        Act: Получение детальной информации о категории.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка совпадения значения 'sku' в полученных данных с ожидаемым.
+        """
         category = Category.objects.get(sku="TestSKU")
         response = self.client.get(f"/api/categories/{category.sku}/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["sku"], "TestSKU")
 
     def test_filter_categories_by_group(self):
-        """Тест фильтрации категорий по группе."""
+        """
+        Act: Фильтрация категорий по полю 'group'.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
         response = self.client.get(
             "http://127.0.0.1:8000/api/categories/?group=TestCategoryGroup"
         )
@@ -48,7 +66,151 @@ class CategoryAPITestCase(TestCase):
         self.assertEqual(len(response.data), 1)
 
     def test_filter_categories_by_uom(self):
-        """Тест фильтрации категорий по uom."""
+        """
+        Act: Фильтрация категорий по полю 'uom'.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
         response = self.client.get("http://127.0.0.1:8000/api/categories/?uom=2")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
+
+
+class StoreAPITestCase(TestCase):
+    """Тестирование StoreViewSet."""
+
+    def setUp(self):
+        """Создание клиента API и тестовых данных магазина."""
+        self.client = APIClient()
+        self.store_data = {
+            "store": "TestStore",
+            "city": "TestCity",
+            "division": "TestDivision",
+            "type_format": 1,
+            "loc": 1,
+            "size": 1,
+            "is_active": True,
+        }
+        self.store = Store.objects.create(**self.store_data)
+
+    def test_get_store_list(self):
+        """
+        Act: Получение списка магазинов.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get("/api/shops/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_get_store_detail(self):
+        """
+        Act: Получение детальной информации о магазине.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get(f"/api/shops/{self.store.store}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["store"], "TestStore")
+
+    def test_filter_stores_by_type_format(self):
+        """
+        Act: Фильтрация магазинов по типу формата.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get("/api/shops/?type_format=1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_filter_stores_by_loc(self):
+        """
+        Act: Фильтрация магазинов по местоположению.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get("/api/shops/?loc=1")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_filter_stores_by_city(self):
+        """
+        Act: Фильтрация магазинов по городу.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get("/api/shops/?city=TestCity")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_filter_stores_by_division(self):
+        """
+        Act: Фильтрация магазинов по подразделению.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 1).
+        """
+        response = self.client.get("/api/shops/?division=TestDivision")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_invalid_type_format_value(self):
+        """
+        Act: Попытка фильтрации магазинов по неправильному значению типа формата.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 0).
+        """
+        response = self.client.get("/api/shops/?type_format=999")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
+
+    def test_invalid_loc_value(self):
+        """
+        Act: Попытка фильтрации магазинов по неправильному значению местоположения.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 0).
+        """
+        response = self.client.get("/api/shops/?loc=999")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
+
+    def test_invalid_city_value(self):
+        """
+        Act: Попытка фильтрации магазинов по неправильному значению города.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 0).
+        """
+        response = self.client.get("/api/shops/?city=Nonexist")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
+
+    def test_invalid_division_value(self):
+        """
+        Act: Попытка фильтрации магазинов по неправильному значению подразделения.
+
+        Assert:
+        - Проверка статуса ответа (HTTP 200 OK).
+        - Проверка количества полученных данных (должно быть 0).
+        """
+        response = self.client.get("/api/shops/?division=Nonexist")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
